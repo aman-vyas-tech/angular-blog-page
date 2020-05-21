16730c56cacbc4fe981163a1796584ce43fceee4
Aman Vyas <1557282@TCS.com>
2020-05-21 14:54:25 +0530

    Implementation of blogsite in angular

--- blog-assignment/src/app/app.module.ts
+++ blog-assignment/src/app/app.module.ts
@@ -1,9 +1,13 @@
+//Core Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
+
+//Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PostComponent } from './components/post/post.component';
@@ -24,7 +28,8 @@ import { LoaderInterceptor } from './shared/loader.interceptor';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatButtonModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
